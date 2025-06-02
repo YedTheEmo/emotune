1487b7e3f2523d9cd4701acb1ad116ba4b242395
HawtStrokes <hawtstrokes1337@gmail.com>
2025-06-02 19:08:29 +0800

    Reset main branch for new project

--- emotune/music_engine/__init__.py
+++
@@ -1,4 +0,0 @@
-# music_engine/__init__.py
-# Empty or just:
-# from .music_controller import MusicController
-
--- run.py
+++
@@ -1,7 +0,0 @@
-from emotune import create_app  # Import create_app() from __init__.py
-
-app = create_app()  # Initialize Flask app
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)  # Start Flask server
-
